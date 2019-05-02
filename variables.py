af3639e5e80efc97a8e6e0aac3fec58859edda24
akhil0182 <40930539+akhil0182@users.noreply.github.com>
2019-05-02 18:12:49 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,7 @@
+def initialize():     
+    global download_count      
+    download_count = 0
+    global prev_download_count
+    prev_download_count = 0
+    global curr_download_count
+    curr_download_count=0
