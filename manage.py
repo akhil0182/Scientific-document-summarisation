acdd29e8b3c2e71bc9c3c9a9805d188977dd4fef
akhil0182 <40930539+akhil0182@users.noreply.github.com>
2019-05-02 18:11:07 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,8 +1,15 @@
+import variables
+variables.initialize()
 import pdf2txt
 import extractor
 import pdf_downloader
-import os 
+#from pdf_downloader import download_count
+import os
 os.system('python pdf2txt.py base.pdf')
-extractor.extrac()
-pdf_downloader.download()
+#extractor.extrac()
+#pdf_downloader.download()
 
+
+        
+                
+   
